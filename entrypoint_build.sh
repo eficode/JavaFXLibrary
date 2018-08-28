67e0c906b3e2f1e190e27e526d47da309ee230be
Hoisko Sakari.M 10802213 <sakari.hoisko@eficode.com>
2018-08-28 17:07:42 +0300

    Added needed privialedges to run

--- entrypoint_build.sh
+++ entrypoint_build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 rm -rvf /javafxbinary/*
 cp -vf /javafxlibrary-*-jar-with-dependencies.jar /javafxbinary/.
+chmod 555 /javafxbinary/javafxlibrary-*-jar-with-dependencies.jar
 java -jar /javafxlibrary-*-jar-with-dependencies.jar
